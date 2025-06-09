049c93d23f9ff5d82036f22aba51cfef71f40c6b
William David Gazabon <78979753+heroelocopro@users.noreply.github.com>
2025-06-08 22:24:19 -0500

    deploy10

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,10 @@ cd /var/www/html
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R 775 storage bootstrap/cache
 
-# 2. Configurar entorno si no existe
-if [ ! -f ".env" ]; then
-    cp .env.example .env
-    php artisan key:generate
-fi
 
-# 3. Esperar a que PostgreSQL esté disponible
-echo "Esperando a PostgreSQL..."
-while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME -d $DB_DATABASE -t 1 >/dev/null 2>&1; do
-    sleep 2
-done
 
-# 4. Limpieza inicial
-php artisan config:clear
-php artisan view:clear
-php artisan event:clear
+
+
 
 # 5. Optimización
 php artisan optimize
