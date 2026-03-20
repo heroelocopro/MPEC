3a6e2d40d13e96fbd5463adb8ebc3b18076d421f
william david gazabon galvis <williamdavidgazabon@gmail.com>
2026-03-19 21:26:00 -0500

    update:filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,11 +56,10 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'auto'),
+            'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' =>env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => true,
+            'use_path_style_endpoint' => false,
         ],
 
 
